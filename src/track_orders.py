7f690e241dbee4f2ee5b4824e0b752a9446f0cbd
matheusnff85 <math.marinho154@gmail.com>
2023-02-14 13:27:54 -0300

    feat: implementa requisito 2 track orders

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,76 @@
 class TrackOrders:
+    def __init__(self):
+        self._data = []
+        self._dishes = set()
+        self._days = set()
+
     # aqui deve expor a quantidade de estoque
     def __len__(self):
-        pass
+        return len(self._data)
+
+    def all_dishes(self):
+        for dish in self._data:
+            self._dishes.add(dish["order"])
+
+    def open_days(self):
+        for day in self._data:
+            self._days.add(day["day"])
 
     def add_new_order(self, customer, order, day):
-        pass
+        self._data.append(
+            {"customer": customer, "order": order, "day": day}
+        )
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        frequency = {}
+
+        for order in self._data:
+            if order["customer"] == customer:
+                if order["order"] not in frequency:
+                    frequency[order["order"]] = 1
+                else:
+                    frequency[order["order"]] += 1
+
+        return max(frequency, key=frequency.get)
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        already_ordered = set()
+        self.all_dishes()
+
+        for order in self._data:
+            if order["customer"] == customer:
+                already_ordered.add(order["order"])
+
+        return self._dishes.difference(already_ordered)
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        self.open_days()
+        already_went = set()
+
+        for order in self._data:
+            if order["customer"] == customer:
+                already_went.add(order["day"])
+
+        return self._days.difference(already_went)
 
     def get_busiest_day(self):
-        pass
+        busiest_day = {}
+
+        for order in self._data:
+            if order["day"] not in busiest_day:
+                busiest_day[order["day"]] = 1
+            else:
+                busiest_day[order["day"]] += 1
+
+        return max(busiest_day, key=busiest_day.get)
 
     def get_least_busy_day(self):
-        pass
+        lazyest_day = {}
+
+        for order in self._data:
+            if order["day"] not in lazyest_day:
+                lazyest_day[order["day"]] = 1
+            else:
+                lazyest_day[order["day"]] += 1
+
+        return min(lazyest_day, key=lazyest_day.get)
